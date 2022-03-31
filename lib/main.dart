94a8d4a0d0be4c14779f22835e9a0f6c4ffd0bbb
sooryasrajan <soorya.s27@gmail.com>
2022-03-31 12:12:21 +0530

    Added firebase stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,42 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_example/screens/landing_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  var heartRate = FirebaseDatabase.instance.ref("heartrate");
+
+  heartRate.onValue.listen((DatabaseEvent event){
+    print(event.snapshot.value);
+  });
+
+  var oxygen = FirebaseDatabase.instance.ref("oxygen");
+
+  oxygen.onValue.listen((DatabaseEvent event){
+    print(event.snapshot.value);
+  });
+
+  /// Firebase FireStore
+  // CollectionReference _collectionRef =
+  // FirebaseFirestore.instance.collection('heartrate');
+  // Future<void> getData() async {
+  //   // Get docs from collection reference
+  //   QuerySnapshot querySnapshot = await _collectionRef.get();
+  //
+  //   // Get data from docs and convert map to List
+  //   final allData = querySnapshot.docs.map((doc) => doc.data()).toList();
+  //
+  //   print(allData);
+  // }
+  // getData();
+
+
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: LandingPage(),
